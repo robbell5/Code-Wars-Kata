487917a342c8c11f06227a8574a14360a84d9f17
Rob <rob.bell.86@gmail.com>
2016-01-09 15:26:48 -0500

    FilterCoffee -- Not working in codewars, test cases not correct on their side.

---
+++ Code Wars Kata/Code Wars Kata Tests/FilterCoffeeTest/FilterCoffeeTests.cs
@@ -0,0 +1,21 @@
+﻿using Code_Wars_Kata.FilterCoffee;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace Code_Wars_Kata_Tests.FilterCoffeeTest
+{
+    [TestFixture]
+    public class FilerCoffeeTests
+    {
+        [Test]
+        [TestCase(3, new int[] { 6, 1, 2, 9, 2 }, new int[] { 1, 2, 2 })]
+        [TestCase(14, new int[] { 7, 3, 23, 9, 14, 20, 7 }, new int[] { 3, 7, 7, 9, 14 })]
+        [TestCase(0, new int[] { 6, 1, 2, 9, 2 }, new int[] { })]
+        public void FixedTest(int budget, int[] prices, int[] expectedResults)
+        {
+            int[] results = FilterCoffee.Search(budget, prices);
+            Assert.AreEqual(expectedResults, results);
+        }
+    }
+}
---
+++ Code Wars Kata/Code Wars Kata/FilterCoffee/FilterCoffee.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Code_Wars_Kata.FilterCoffee
+{
+    public class FilterCoffee
+    {
+        public static int[] Search(int budget, int[] prices)
+        {
+            List<int> affordablePrices = prices.Where(price => price <= budget).ToList();
+            affordablePrices.Sort();
+            return affordablePrices.ToArray();
+        }
+    }
+}
