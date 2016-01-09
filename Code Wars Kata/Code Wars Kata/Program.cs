7ae256f2b58972e550d31e293b2346e1a2177925
Rob <rob.bell.86@gmail.com>
2016-01-09 14:25:28 -0500

    Test Commit

---
+++ Code Wars Kata/Code Wars Kata/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Wars_Kata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //test
+            string something = "asd";
+        }
+    }
+}
